c2abb01421743e1073d2bf4d8b3aea51cdc17db0
selfstudymaster <webselfmaster@gmail.com>
2020-12-12 16:43:19 +0900

    Adding ListView.builder and ListView.separated

--- list_view_flutter_app/lib/main.dart
+++ list_view_flutter_app/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-/* 事前に出力するデータが揃ってる場合その1
+// 事前に出力するデータが揃ってる場合その1
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
 }
 */
 
-/* 事前に出力するデータが揃ってる場合その2
+// 事前に出力するデータが揃ってる場合その2
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -73,6 +75,8 @@ class MyApp extends StatelessWidget {
 }
 */
 
+// 表示する要素が事前にわからない場合 ListView.builder
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -117,90 +121,103 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
 
+// ListView.separated
+// 表示する要素が事前にわかっており、一行ごとに何か要素を差し込みたい場合に利用
 /*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ", "メッセージ","メッセージ","メッセージ","メッセージ",];
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('ListView'),
+            ),
+            body: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return _messageItem(list[index]);
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return separatorItem();
+              },
+              itemCount: list.length,
+            )
+        )
+    );
+  }
+  Widget separatorItem() {
+    return Container(
+      height: 10,
+      color: Colors.orange,
+    );
+  }
+
+  Widget _messageItem(String title) {
+    return Container(
+      decoration: new BoxDecoration(
+          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      child:ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+              color:Colors.black,
+              fontSize: 18.0
+          ),
         ),
+        onTap: () {
+          print("onTap called.");
+        }, // タップ
+        onLongPress: () {
+          print("onLongTap called.");
+        }, // 長押し
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 */
+
+// ScrollDirection
+// スクロールの方向を変える
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var list = ["0","1","2","3","4","5","6","7","8","9"];
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('ListView'),
+            ),
+            body: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                if (index >= list.length) {
+                  list.addAll(["0","1","2","3","4","5","6","7","8","9",]);
+                }
+                return _messageItem(list[index]);
+              },
+            )
+        )
+    );
+  }
+
+  Widget _messageItem(String title) {
+    return Container(
+        width: 100,
+        decoration: new BoxDecoration(
+            border: new Border(right: BorderSide(width: 1.0, color: Colors.grey))
+        ),
+        child:Center(
+          child:Text(
+            title,
+            style: TextStyle(
+                color:Colors.black,
+                fontSize: 20.0
+            ),
+          ),
+        )
+    );
+  }
+}
